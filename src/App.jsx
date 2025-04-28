41eb07f073faf71b5f3e0010439f9dabd72356d3
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-04-28 15:44:11 +0100

    update readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import reactLogo from './assets/react.svg';
+import reactLogo from "./assets/react.svg";
 
 const MainBody = () => {
-    return (
-        <main>
-            <img src = {reactLogo} alt="react logo"/>
-            <h1>Fun facts about react</h1>
-            <ul>
-                <li>Was first release in 2013</li>
-                <li>Was originally created by jordan walke</li>
-                <li>Has well over 200k stars on Github</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </main>
-    )
-}
+  return (
+    <main>
+      <img src={reactLogo} alt="react logo" />
+      <h1>Fun facts about react</h1>
+      <ul>
+        <li>Was first release in 2013</li>
+        <li>Was originally created by jordan walke</li>
+        <li>Has well over 200k stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+};
 
 const App = () => {
-    return (
-        < MainBody />
-    )
-}
+  return <MainBody />;
+};
 
-export default App
+export default App;
