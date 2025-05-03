c81ea53d358d144923960c97d8ec737e0afafe95
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-05-04 00:41:20 +0100

    understanding props destructuring

--- src/Components/contact cat.jsx
+++ src/Components/contact cat.jsx
@@ -1,12 +1,12 @@
-const Contact = (props) => {
+const Contact = (name, phoneNumber, email) => {
   return (
     <div className="w-55 bg-sky-400 p-2 rounded-lg">
       <div className="w-full h-40 bg-black rounded-md"></div>
-      <p className="mt-2 text-xl font-medium text-white">{props.name}</p>
+      <p className="mt-2 text-xl font-medium text-white">{name}</p>
 
       <div className="mt-4 space-y-2 text-white">
-        <p>{props.phoneNumber}</p>
-        <p>{props.email}</p>
+        <p>{phoneNumber}</p>
+        <p>{email}</p>
       </div>
     </div>
   );
