0dc6da8e54072aea0f85f5eb8e50d646228f60dc
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-05-04 01:06:54 +0100

    explaining to myself

--- src/Components/joke.jsx
+++ src/Components/joke.jsx
@@ -1,5 +1,6 @@
 const Joke = (props) => {
   return (
+    // {if props.Setup if truthy then(which is the &&) will run the next code <h1>}
     <>
       {props.Setup && <h1>Setup: {props.Setup}</h1>}
       <p>
