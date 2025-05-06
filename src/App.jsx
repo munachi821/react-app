6c80e12b37a075ff080661db851a18dc9e00b020
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-05-06 11:44:20 +0100

    chef claude beginner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
-//import Main from "./Components/Main";
-//import JokesData from "./jokesData";
-//import Joke from "./Components/joke";
-import Header from "./Components/Header";
-import Entry from "./Components/entry";
-import Appdata from "./Components/data.js";
-import mountfujiimg from "./images/mount fuji.jpg";
-
 const App = () => {
-  const Data = Appdata.map((entrydata) => {
-    return (
-      <Entry
-        src={entrydata.img.src}
-        alt={entrydata.img.src}
-        title={entrydata.title}
-        country={entrydata.country}
-        date={entrydata.dates}
-        text={entrydata.text}
-      />
-    );
-  });
-  return (
-    <>
-      <Header />
-      <div className="space-y-5">{Data}</div>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
--- src/Components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-red-400 w-full py-5 text-center text-white">
-      <p>my travel journal.</p>
-    </header>
-  );
-}
