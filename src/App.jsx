9bdc18e5f4548941f1a4bdf6cd470b3ad4d82b0a
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-05-16 14:50:10 +0100

    forms begining

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import React from "react";
 import avatar from "./images/user.png";
 import starFilled from "./images/star-filled.png";
 import starEmpty from "./images/star-empty.png";
-import "./Components/";
+import "./Components/index.css";
 
 export default function App() {
   const [contact, setContact] = React.useState({
@@ -23,16 +23,22 @@ export default function App() {
     lastName: "Doe",
     phone: "+1 (212) 555-1212",
     email: "itsmyrealname@example.com",
-    isFavorite: false,
+    isFavorite: true,
   });
   /**
    * Challenge: Fill in the values in the markup
    * using the properties of our state object above
    * (Ignore `isFavorite` for now)
    */
+  let starIcon = contact.isFavorite ? starFilled : starEmpty;
 
   function toggleFavorite() {
-    console.log("Toggle Favorite");
+    setContact((prevContact) => {
+      return {
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite,
+      };
+    });
   }
 
   return (
@@ -46,14 +52,23 @@ export default function App() {
         <div className="info">
           <button
             onClick={toggleFavorite}
-            aria-pressed={false}
+            aria-pressed={contact.isFavorite}
+            aria-label={
+              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+            }
             className="favorite-button"
           >
-            <img src={starEmpty} alt="empty star icon" className="favorite" />
+            <img
+              src={starIcon}
+              alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
+              className="favorite"
+            />
           </button>
-          <h2 className="name">John Doe</h2>
-          <p className="contact">+1 (212) 555-1212</p>
-          <p className="contact">itsmyrealname@example.com</p>
+          <h2 className="name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="contact">{contact.phone}</p>
+          <p className="contact">{contact.email}</p>
         </div>
       </article>
     </main>
