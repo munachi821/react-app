1bad2439d1a8780214f3a1b152d4ac95a50b6037
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-05-16 15:32:12 +0100

    moving onto the next screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,39 @@
-/* import Header from "./Components/Header";
+import Header from "./Components/Header";
 import Main from "./Components/Main";
+import "./Components/index.css";
 const App = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget;
+    const formData = new FormData(formEl);
+    const email = formData.get("email");
+    formEl.reset();
+  }
   return (
     <>
-      <Header />
-      <Main />
+      {/* <Header />
+      <Main /> */}
+      <section>
+        <h1>Signup Form</h1>
+        <form method="post" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            id="email"
+          />
+          <br />
+
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <br />
+
+          <button>Submit</button>
+        </form>
+      </section>
     </>
   );
 };
 
 export default App;
- */
-import React from "react";
-import avatar from "./images/user.png";
-import starFilled from "./images/star-filled.png";
-import starEmpty from "./images/star-empty.png";
-import "./Components/index.css";
-
-export default function App() {
-  const [contact, setContact] = React.useState({
-    firstName: "John",
-    lastName: "Doe",
-    phone: "+1 (212) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: true,
-  });
-  /**
-   * Challenge: Fill in the values in the markup
-   * using the properties of our state object above
-   * (Ignore `isFavorite` for now)
-   */
-  let starIcon = contact.isFavorite ? starFilled : starEmpty;
-
-  function toggleFavorite() {
-    setContact((prevContact) => {
-      return {
-        ...prevContact,
-        isFavorite: !prevContact.isFavorite,
-      };
-    });
-  }
-
-  return (
-    <main>
-      <article className="card">
-        <img
-          src={avatar}
-          className="avatar"
-          alt="User profile picture of John Doe"
-        />
-        <div className="info">
-          <button
-            onClick={toggleFavorite}
-            aria-pressed={contact.isFavorite}
-            aria-label={
-              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
-            }
-            className="favorite-button"
-          >
-            <img
-              src={starIcon}
-              alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-              className="favorite"
-            />
-          </button>
-          <h2 className="name">
-            {contact.firstName} {contact.lastName}
-          </h2>
-          <p className="contact">{contact.phone}</p>
-          <p className="contact">{contact.email}</p>
-        </div>
-      </article>
-    </main>
-  );
-}
