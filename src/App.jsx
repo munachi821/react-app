2629a7b3818f8d7ab537e280e5f1a3c1c0acc7af
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-05-16 17:42:43 +0100

    done with the form learning time for implementing

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
 import Header from "./Components/Header";
 import Main from "./Components/Main";
-import "./Components/index.css";
 const App = () => {
-  function signUp(formData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
-    console.log(email);
-    console.log(password);
-  }
   return (
     <>
-      {/* <Header />
-      <Main /> */}
-      <section>
-        <h1>Signup Form</h1>
-        <form action={signUp}>
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="johndoe@gmail.com"
-            id="email"
-          />
-          <br />
-
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-          <br />
-
-          <button>Submit</button>
-        </form>
-      </section>
+      <Header />
+      <Main />
     </>
   );
 };
