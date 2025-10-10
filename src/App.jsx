732357b46f6ff1b84874d1764b0dcde68a80dbfe
munachi821 <148701522+munachi821@users.noreply.github.com>
2025-10-10 11:10:48 +0100

    installed nanoid an solved some coding challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import Die from "./Components/Die";
 const App = () => {
+  const [tennumbers, setTenNumbers] = useState(generateAllNewDice());
+  function generateAllNewDice() {
+    let randomNum = new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6));
+
+    let objNum = randomNum.map((num) => ({
+      value: num,
+      isHeld: false,
+    }));
+    return objNum;
+  }
+
+  function rollDice() {
+    setTenNumbers(generateAllNewDice());
+    console.log(tennumbers);
+  }
+
+  //const diceElements = tennumbers.map((num) => <Die value={num} />);
   return (
-    <main>
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
-      <Die value={1} />
+    <main className="bg-black flex items-center justify-center h-screen p-4">
+      <div className="bg-[#f5f5f5] w-full h-full rounded-xl flex items-center justify-center flex-col">
+        <div className="grid grid-cols-5 mx-auto gap-7">
+          {tennumbers.map((num) => (
+            <Die value={num.value} />
+          ))}
+        </div>
+        <button
+          className="bg-[#5035ff] text-white px-11 py-2.5 rounded-md mt-6 cursor-pointer"
+          onClick={() => rollDice()}
+        >
+          Roll
+        </button>
+      </div>
     </main>
   );
 };
--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,4 +1,11 @@
 const Die = (props) => {
-  return <button>{props.value}</button>;
+  return (
+    <button
+      className="size-14 rounded-md shadow-md text-2xl font-bold bg-white cursor-pointer"
+      onClick={(e) => console.log(props.value)}
+    >
+      {props.value}
+    </button>
+  );
 };
 export default Die;
